fe265590bfe2ac7e9655c74ee07993e46fb73cd5
rubenphi <rubenphii@gmail.com>
2021-12-25 19:21:08 +0000

    intento de cambio de titulo

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,10 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '',
-    redirect: '/folder/Inbox'
+    redirect: '/Home'
   },
   {
-    path: '/folder/:id',
+    path: '/Home',
     component: () => import ('../views/FolderPage.vue')
   }
 ]
